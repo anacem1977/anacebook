8432a60e49fdb041db4d377135cc71e6bfdce4f4
anace-m <maldonadoanaceciliac@johndeere.com>
2021-02-12 11:15:07 -0600

    created friends page

---
+++ src/components/FriendsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FriendsPage = (props) => {
+    return (
+        <div>
+            <h2>This is my friends list</h2>
+        </div>
+
+    )
+}
+
+export default FriendsPage;
